71c2a5d416b00931db24cb399bbae329decd3640
Mayur28 <mayur.28.ranchod@gmail.com>
2020-08-01 12:02:49 +0200

    Update SetupTraining.py

--- MyResearch/SetupTraining.py
+++ MyResearch/SetupTraining.py
@@ -1,29 +1,29 @@
 import os
 import torch
-from util import util
 import argparse
 
 class SetupTraining():
-	def __init__(self):
-		self.parser=argparse=ArgumentParser()
-		# These are the BaseOptions (Change the 'help' eventually!)
-		self.parser.add_argument('--dataroot', required=True, help='path to images (should have subfolders trainA, trainB, valA, valB, etc)')# Remove
+    def __init__(self):
+        self.parser=argparse.ArgumentParser()
+        # These are the BaseOptions (Change the 'help' eventually!)
+        #self.parser.add_argument('--dataroot', required=True, help='path to images (should have subfolders trainA, trainB, valA, valB, etc)')
         self.parser.add_argument('--batchSize', type=int, default=1, help='input batch size (One of the aspects that can be used to control GPU requirements)')
         self.parser.add_argument('--crop_size', type=int, default=256, help='Crop the images to this new size')
         self.parser.add_argument('--patch_size', type=int, default=64, help='specifies the size of the patch that we are going to use')
         self.parser.add_argument('--gpu_ids', type=str, default='0', help="Used to specify the ID's of the GPU's to be used (e.g 0,1 if 2 GPU's are used)")
-		# Ours will alls be in accordance with the unaligned dataset
-		# We are only using the single_model
-		# Set the number of loading threads to 6 ( there's is 4)
-        self.parser.add_argument('--checkpoints_dir', type=str, default='/content/drive/My Drive/EnlightenGAN2/EnlightenGAN-master/checkpoints/', help='models are saved here')
+        #Ours will alls be in accordance with the unaligned dataset
+        # We are only using the single_model
+        # Set the number of loading threads to 6 ( there's is 4)
+        #self.parser.add_argument('--checkpoints_dir', type=str, default='/content/drive/My Drive/EnlightenGAN2/EnlightenGAN-master/checkpoints/', help='models are saved here')
+        self.parser.add_argument('--checkpoints_dir', type=str, default='/content/drive/My Drive/MyResearch', help='models are saved here')
         self.parser.add_argument('--norm', type=str, default='instance', help='instance normalization or batch normalization')
-		# We are only cropping, experiment with the other options here!
+        # We are only cropping, experiment with the other options here!
         self.parser.add_argument('--resize_or_crop', type=str, default='crop', help='scaling and cropping of images at load time [resize_and_crop|crop|scale_width|scale_width_and_crop]')
         self.parser.add_argument('--skip', type=float, default=0.8, help='B = net.forward(A) + skip*A')
         self.parser.add_argument('--use_mse', action='store_true', help='MSELoss')
-		# use_norm will be set to true by default
+        # use_norm will be set to true by default
         self.parser.add_argument('--use_ragan', action='store_true', help='use ragan')
-		#Sort out the VGG stuff!
+        #Sort out the VGG stuff!
         self.parser.add_argument('--vgg', type=float, default=0, help='use perceptrual loss')
         self.parser.add_argument('--vgg_mean', action='store_true', help='substract mean in vgg loss')
         self.parser.add_argument('--vgg_choose', type=str, default='relu5_3', help='choose layer for vgg')
@@ -32,40 +32,36 @@ class SetupTraining():
         self.parser.add_argument('--IN_vgg', action='store_true', help='patch vgg individual')
         self.parser.add_argument('--use_avgpool', type=float, default=0, help='use perceptrual loss')
         self.parser.add_argument('--instance_norm', type=float, default=0, help='use instance normalization')
-		# I want tanh at the end of mine!
+        # I want tanh at the end of mine!
         self.parser.add_argument('--tanh', action='store_true', help='tanh')
-		# noise will be set to 0 by default
-		#input_linear is false
-		#patchD will be True by default
+        # noise will be set to 0 by default
+        #input_linear is false
+        #patchD will be True by default
         self.parser.add_argument('--patchD_3', type=int, default=0, help='choose the number of crop for patch discriminator')
-		# We use D_P_times2, but look into it further. The vgg stuff can be considerably simplified.
+        # We use D_P_times2, but look into it further. The vgg stuff can be considerably simplified.
         self.parser.add_argument('--D_P_times2', action='store_true', help='loss_D_P *= 2')
         self.parser.add_argument('--patch_vgg', action='store_true', help='use vgg loss between each patch')
         self.parser.add_argument('--hybrid_loss', action='store_true', help='use lsgan and ragan separately')
         self.parser.add_argument('--self_attention', action='store_true', help='adding attention on the input of generator')
-		# We have this!
+        # We have this!
         self.parser.add_argument('--times_residual', action='store_true', help='output = input + residual*attention')
-		
-		#Now, the proper training options
-		self.parser.add_argument('--print_freq', type=int, default=100, help='frequency of showing training results on console')
+        #Now, the proper training options
+        self.parser.add_argument('--print_freq', type=int, default=100, help='frequency of showing training results on console')
         self.parser.add_argument('--save_latest_freq', type=int, default=5000, help='frequency of saving the latest results')
         self.parser.add_argument('--save_epoch_freq', type=int, default=5, help='frequency of saving checkpoints at the end of epochs')
-		self.parser.add_argument('--phase', type=str, default='train', help='train, val, test, etc')
-		self.parser.add_argument('--niter', type=int, default=100, help='# of iter at starting learning rate')
+        self.parser.add_argument('--phase', type=str, default='train', help='train, val, test, etc')
+        self.parser.add_argument('--niter', type=int, default=100, help='# of iter at starting learning rate')
         self.parser.add_argument('--niter_decay', type=int, default=100, help='# of iter to linearly decay learning rate to zero')
         self.parser.add_argument('--beta1', type=float, default=0.5, help='momentum term of adam')
         self.parser.add_argument('--lr', type=float, default=0.0001, help='initial learning rate for adam')
-		
-		self.parser.add_argument('--no_lsgan', action='store_true', help='do *not* use least square GAN, if false, use vanilla GAN')
+        self.parser.add_argument('--no_lsgan', action='store_true', help='do *not* use least square GAN, if false, use vanilla GAN')
         self.parser.add_argument('--pool_size', type=int, default=50, help='the size of image buffer that stores previously generated images')
-
-		self.parser.add_argument('--config', type=str, default='configs/unit_gta2city_folder.yaml', help='Path to the config file.')
+        self.parser.add_argument('--config', type=str, default='configs/unit_gta2city_folder.yaml', help='Path to the config file.')
         self.isTrain = True
-		self.parse()
-		
+        self.parse()
+        
 
-	def parse(self):
-
+    def parse(self):
         self.opt = self.parser.parse_args()
         self.opt.isTrain = self.isTrain   # train or test
 
@@ -88,8 +84,9 @@ class SetupTraining():
         print('-------------- End ----------------')
 
         # save to the disk
-        expr_dir = os.path.join(self.opt.checkpoints_dir, self.opt.name)
-        util.mkdirs(expr_dir)
+        expr_dir = os.path.join(self.opt.checkpoints_dir, "TheModel")
+        if(os.path.isdir(expr_dir)==False):
+            os.mkdir(expr_dir)
         file_name = os.path.join(expr_dir, 'opt.txt')
         with open(file_name, 'wt') as opt_file:
             opt_file.write('------------ Options -------------\n')
@@ -97,12 +94,3 @@ class SetupTraining():
                 opt_file.write('%s: %s\n' % (str(k), str(v)))
             opt_file.write('-------------- End ----------------\n')
         return self.opt
-
-		
-
-
-		
-		
-		
-		
-		
