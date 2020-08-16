b8882fb731f5455b05b0adaf198313dd4d2e82f2
Mayur28 <mayur.28.ranchod@gmail.com>
2020-08-16 16:53:21 +0200

    ...

--- MyResearch/train.py
+++ MyResearch/train.py
@@ -1,123 +1,58 @@
-import os
-import torch
-import torch.utils.data as data
-import cv2
-import imghdr
-import torchvision.transforms as transforms# Try to remove
+from SetupTraining import SetupTraining
+from ManageData import DataLoader
 from PIL import Image
-import numpy as np
-import matplotlib.pyplot as plt
+import Networks
+import time
+import os
 
 
-def DataLoader(opt):
-    data_loader=DataLoader(opt)
-    return data_loader
+def display_current_results(images,epoch):
+    for label,image in images.items():
+        img_path=os.path.join(opt.img_dir,'epoch%.3d_%s.png'%(epoch,label))
+        image_pil=Image.fromarray(image)
+        image_pil.save(img_path)
 
-def MakeDataset(opt):
-    dataset=FullDataset(opt)
-    return dataset
-
-def import_dataset(directory):
-    images = []# This seems to  be the way to go...
-    for root, _, files in sorted(os.walk(directory)):
-        for file_name in files:
-            if  imghdr.what(directory+"/"+file_name)=='png' or  imghdr.what(directory+"/"+file_name)=='jpeg':
-                path = os.path.join(root, file_name)
-                img =Image.open(path).convert('RGB')# cv2.imread(path,1)# Will be read in BGR!
-                images.append(img)
-    return images
-
-def config_transforms(opt):# I Should account for other kinds of transforms such as resize and crop (THIS IS WHERE MY DATA AUGMENTATION WILL GO. RIGHT NOW, IM ONLY DOING RANDOM CROPPING)
-    trans_list=[]
-    trans_list.append(transforms.RandomCrop(opt.crop_size))
-
-    #To normalize the data to the range [-1,1]
-    trans_list+=[transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5),(0.5, 0.5, 0.5))]# this is neat. Looking at one channel (column), we are specifying mean=std=0.5 which normalizes the images to [-1,1]
-    return transforms.Compose(trans_list)
-
-class DataLoader:
-    def __init__(self,opt):
-        self.opt=opt
-        self.dataset=MakeDataset(opt)
-        self.dataloader= torch.utils.data.DataLoader(self.dataset,batch_size=opt.batch_size,shuffle= True, num_workers=6)
-
-    def load(self):# This will return the iterable over the dataset
-        return self.dataloader
-
-    #This function is compulsory when creating custom dataloaders!
-    def __len__(self):
-        return len(self.dataset)
-
-    #Try to implement a __getitem__ to extract individual instances as well!
+def print_errors(epoch,i,errors,t):
+    message='(epoch: %d, iters: %d, time: %.3f)'%(epoch,i,t)
+    for k,v in errors.items():# --> This is to extract from the Ordered Dictionary
+        message+= '%s: %.3f ' % (k, v)
+    print(message)
+    with open(opt.log_name, "a") as log_file:
+        log_file.write('%s\n' % message)
 
 
-class FullDataset(data.Dataset):# I've inherited what I had to
-    # This class definitely needs to overwrite __len__ and  __getitem__
-    def __init__(self,opt):
-        super(FullDataset, self).__init__()
-        self.opt=opt
-        #Form the path's to the data
-        A_directory=os.path.join('../final_dataset',opt.phase+'A')
-        B_directory=os.path.join('../final_dataset',opt.phase+'B')
+opt=SetupTraining().process()
+data_loader=DataLoader(opt)
+dataset=data_loader.load()
+print("Number of training Images: %d"% len(data_loader))
+the_model=Networks.The_Model(opt)
 
+total_steps=0
 
-        self.A_imgs = import_dataset(A_directory)
-        self.B_imgs = import_dataset(B_directory)
+# Below is the big deal!!! range(1,100+100+1)# the lr decays for last 100 epochs
+for epoch in range(1,opt.niter+opt.niter_decay+1):
+    epoch_start_time=time.time()
+    for i, data in enumerate(dataset): # For each call, __get_item__ is called for each image in the current batch. Takes the images, formats it into the desired dictionary format, and this dictionary is then represented by data
 
+        iter_start_time=time.time()
+        total_steps+=opt.batch_size
+        epoch_iter=total_steps-len(data_loader)*(epoch-1)
+        #Remember at this stage, data is the batch 'dataset' in dictionary format. It slots the data into the correct variables self.inputA,etc to easily perform propagation operations
+        the_model.perform_update(data)
 
-        self.A_size=len(self.A_imgs)
-        self.B_size=len(self.B_imgs)
-        self.transform=config_transforms(opt)#--> Experiment with data augmentation
+        if(total_steps% opt.display_freq==0):
+            display_current_results(the_model.for_displaying_images(),epoch)
 
-    def __getitem__(self,index):
-        A_img=self.A_imgs[index%self.A_size]# To avoid going out of bounds
-        B_img=self.B_imgs[index% self.B_size]
+        if(total_steps% opt.print_freq==0):
+            exec_time=(time.time()-iter_start_time)/opt.batch_size
+            print_errors(epoch,epoch_iter,the_model.get_model_errors(epoch),exec_time)
 
+    if(epoch% opt.save_epoch_freq==0):
+        #model.save('latest')
+        the_model.save_model(epoch)
 
-        A_img=self.transform(A_img)#This is where we actually perform the transformation
-        B_img=self.transform(B_img)
+    print('End of epoch %d / %d \t Time Taken: %d sec' %
+          (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
 
-        # What is happening is that we are going from a normal 600x400 image ( In the PIL format),
-        #after the transform, the image is resized and converted into a tensor for each i  mage ( resulting size=[3,320,320])
-
-        input_img=A_img
-        #A_gray=cv2.cvtColor(input_img,0)
-        #A_gray=torch.unsqueeze(A_gray,0)
-        r,g,b = input_img[0]+1, input_img[1]+1, input_img[2]+1
-        A_gray = 1. - (0.299*r+0.587*g+0.114*b)/2. #Verified: The weird numbers are for going from RGB to grayscale
-        # Before: 320x320
-        A_gray = torch.unsqueeze(A_gray, 0)#Returns a new tensor with the entire image sqeezed into the 0th dimension/axis
-        # After 1x320x320
-
-
-
-
-        return {'A': A_img, 'B': B_img, 'A_gray': A_gray, 'input_img': input_img}
-
-    def __len__(self):
-        return max(self.A_size,self.B_size)
-
-# Right now, this is directly copied over, NEEDS TO BE REDONE URGENTLY!!!
-# Improve the implementation here( vast amount of rom from improvement here!)
-def TensorToImage(img_tensor,imtype=np.uint8):
-    usable_images=img_tensor[0].cpu().float().numpy()
-    usable_images=(np.transpose(usable_images, (1, 2, 0)) + 1) / 2.0 * 255.0
-    usable_images=np.maximum(usable_images,0)
-    usable_images=np.minimum(usable_images,255)
-    return usable_images.astype(imtype)
-
-# Find out exactly what is going on here( detaching and manipulating???)
-def AttentionToImage(img_tensor,imtype=np.uint8):
-    tensor=img_tensor[0]
-    tensor=torch.cat((tensor, tensor, tensor), 0)
-    usable_images=tensor.cpu().float().numpy()
-    usable_images = (np.transpose(usable_images, (1, 2, 0))) * 255.0
-    usable_images = usable_images/(usable_images.max()/255.0)
-    return usable_images.astype(imtype)
-
-def LatentToImage(img_tensor,imtype=np.uint8):
-    usable_images = img_tensor[0].cpu().float().numpy()
-    usable_images = (np.transpose(usable_images, (1, 2, 0))) * 255.0
-    usable_images = np.maximum(usable_images, 0)
-    usable_images = np.minimum(usable_images, 255)
-    return usable_images.astype(imtype)
+    #if(epoch>opt.niter):
+    #    model.update_learning_rate()
