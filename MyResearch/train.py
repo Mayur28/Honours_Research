df044072e18e5f17acacd2067801f02d9c142e59
Mayur28 <mayur.28.ranchod@gmail.com>
2021-02-02 14:49:27 +0200

    Update train.py

--- MyResearch/train.py
+++ MyResearch/train.py
@@ -5,7 +5,6 @@ import Networks
 import time
 import os
 
-
 # Saves the input and output images
 def save_images(images, title, phase='train'):
     for label, image in images.items():  # .items() extracts the "packages" from the dictionary
